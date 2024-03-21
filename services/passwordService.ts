09bb2fd6e513b34d5594aa5b74fb586b1b95948a
Giovana-Arruda <giovanab.arruda@gmail.com>
2024-03-21 02:23:06 -0300

    feat: services

---
+++ services/passwordService.ts
@@ -0,0 +1,16 @@
+
+export default function generatePass(){
+    let password: string = '';
+    let characteres: string = 'Aa$@#123456789abcdefghijFGHIJKLMNOP!';
+    let passWordLenght = 7;
+
+    for(let index =0; index < passWordLenght; index++){
+        password += characteres.charAt(
+        Math.floor(Math.random() * characteres.length))
+
+    }
+
+
+    return [password, characteres];
+
+}
